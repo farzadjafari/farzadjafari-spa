fb4264d8b68a50fcee525155e1c1583ddb23bff3
farzadjafari <farzad@4thirteen.com.my>
2020-12-08 19:17:36 -0500

    Add pulsing projects

---
+++ resources/js/components/ProjectPulse.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-full sm:w-1/2 md:w-1/3 flex p-2">
+        <div class="border border-gray-300 shadow rounded-md max-w-sm w-full mx-auto">
+            <div class="animate-pulse flex flex-col space-x-4">
+                <div class="bg-gray-400 h-40 w-full"></div>
+
+                <div class="flex-1 space-y-4 p-2">
+                    <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+
+                    <div class="space-y-2">
+                        <div class="h-4 bg-gray-400 rounded"></div>
+
+                        <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
--- resources/js/views/Projects.vue
+++ resources/js/views/Projects.vue
@@ -4,12 +4,15 @@
             <h1 class="text-2xl md:text-3xl lg:text-4xl mb-12 text-center">Projects</h1>
 
             <div class="flex flex-wrap">
-                <project-thumbnail v-for="project in projects" :key="project"
+                <project-pulse v-for="index in 10" :key="index" v-if="isPulsing"></project-pulse>
+
+                <project-thumbnail v-for="(project, i) in projects" :key="i"
                                    :name="project.name"
                                    :slug="project.slug"
                                    :link="project.link"
                                    :why="project.why"
                                    :target="!project.why"
+                                   v-if="!isPulsing"
                 ></project-thumbnail>
             </div>
         </section>
@@ -17,19 +20,24 @@
 </template>
 
 <script>
-import ProjectThumbnail from './../components/ProjectThumbnail.vue'
+
+import ProjectThumbnail from '../components/ProjectThumbnail'
+import ProjectPulse from '../components/ProjectPulse'
 
 export default {
-    components: {ProjectThumbnail},
+    components: {ProjectThumbnail, ProjectPulse},
     data () {
         return {
-            projects: {}
+            projects: {},
+            isPulsing: true,
         }
     },
     created () {
         axios.get('/api/projects')
             .then(response => {
                 this.projects = response.data
+
+                this.isPulsing = false
             })
     },
 }
